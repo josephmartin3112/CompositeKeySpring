ebd2fbfa8609b97dda616934fce4b4b86c904e8a
Joseph Martin <josephmartin3112@gmail.com>
2024-10-04 09:33:15 +0530

    initial commit

---
+++ src/test/java/com/ust/compositekey/compositekey/CompositekeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.compositekey.compositekey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompositekeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
